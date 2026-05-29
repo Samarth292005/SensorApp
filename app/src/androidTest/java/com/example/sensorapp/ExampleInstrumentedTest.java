e9658251920991da79ab6117f8096d0d3fc928d7
Samarth292005 <samarthagarwal6277@gmail.com>
2026-05-29 22:09:29 +0530

    Delete ExampleInstrumentedTest class
    Removed the ExampleInstrumentedTest class and its test method.

--- app/src/androidTest/java/com/example/sensorapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/sensorapp/ExampleInstrumentedTest.java
@@ -1,7 +1,1 @@
 
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.sensorapp", appContext.getPackageName());
-    }
-}
